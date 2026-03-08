d556e4d3eb7c330b42dc7650c0d33c2c63919873
IMfreelunch <260237336+IMfreelunch@users.noreply.github.com>
2026-03-08 15:42:17 +0800

    Remove folders and files to prepare deployment of new repository strcuture

--- api/index.js
+++
@@ -1,8 +0,0 @@
-module.exports = async (req, res) => {
-  res.setHeader('Content-Type', 'application/json');
-  res.status(200).json({ 
-    message: "API working! 🎉",
-    database: process.env.DATABASE_URL ? "✅ Connected" : "❌ Missing",
-    timestamp: new Date().toISOString()
-  });
-};
